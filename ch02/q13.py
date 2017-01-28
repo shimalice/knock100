46b45490e706271105ec10211b67f3bc1d459251
shimalice <shimalicelis@gmail.com>
2017-01-29 02:55:04 +0900

    q13 fix

--- ch02/q13.py
+++ ch02/q13.py
@@ -1,13 +1,15 @@
  # coding: utf-8
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     col1 = open('col1.txt', 'r')
     col2 = open('col2.txt', 'r')
 
     with col1, col2:
-        ary_col1 = col1.readlines
-        ary_col2 = col2.readlines
-        m_col12 = "\n".join([e_col1+"\t"+e_col2 for e_col1, e_col2 in ary_col1, ary_col2])
+        ary_col1 = col1.readlines()
+        # print(ary_col1)
+        ary_col2 = col2.readlines()
+        # print(ary_col2)
+        m_col12 = "\n".join([e_col1.replace('\n', '')+"\t"+e_col2.replace('\n', '') for e_col1, e_col2 in zip(ary_col1, ary_col2)])
 
     with open('m_col.txt', 'w') as m_col:
         m_col.write(m_col12)
